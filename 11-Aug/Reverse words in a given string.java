8d0a3391e9df5feb30f4769f63a402427fed14a6
rakimh46 <60341490+rakimh46@users.noreply.github.com>
2022-08-11 23:09:14 +0530

    Update Reverse words in a given string.java

--- 11-Aug/Reverse words in a given string.java
+++ 11-Aug/Reverse words in a given string.java
@@ -9,3 +9,41 @@ class Solution
         return String.join(".",words);
     }
 }
+
+2nd way
+
+class Solution 
+{
+    //Function to reverse words in a given string.
+    String reverseWords(String S)
+    {
+        // code here 
+        Stack<String> st=new Stack<>();
+        
+        StringBuilder sb=new StringBuilder("");
+        
+        for(int i=0;i<S.length();i++){
+            char ch=S.charAt(i);
+            if(ch!='.'){
+                sb.append(ch);
+            }
+            if(ch=='.'){
+                String str=sb.toString();
+                st.push(str);
+                sb.delete(0,sb.length());
+            }
+        }
+        String str=sb.toString();
+        st.push(str);
+        sb.delete(0,sb.length());
+        while(st.size()>1){
+            str=st.pop();
+            sb.append(str);
+            sb.append(".");
+        }
+        str=st.pop();
+        sb.append(str);
+        return sb.toString();
+    }
+}
+
