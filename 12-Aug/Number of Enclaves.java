d408417f7b7142f1f394f4ce65ce2c4aab29399e
rakimh46 <60341490+rakimh46@users.noreply.github.com>
2022-08-12 21:54:01 +0530

    Update Number of Enclaves.java

--- 12-Aug/Number of Enclaves.java
+++ 12-Aug/Number of Enclaves.java
@@ -30,3 +30,44 @@ class Solution {
         
             }
 }
+
+
+// 2nd way
+
+class Solution {
+    int n;
+    int m;
+    public int numEnclaves(int[][] grid) {
+        n=grid.length;
+        m=grid[0].length;
+        int count=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    int[][] dir={{0,1},{1,0},{-1,0},{0,-1}};
+    public void dfs(int a[][], int i, int j) {
+        if(i >= 0 && i <= a.length - 1 && j >= 0 && j <= a[i].length - 1 && a[i][j] == 1) {
+            a[i][j] = 0;
+            dfs(a, i + 1, j);
+            dfs(a, i - 1, j);
+            dfs(a, i, j + 1);
+            dfs(a, i, j - 1);
+        }
+    }
+}
